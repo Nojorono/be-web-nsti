3f25040383648951c6571c05e5ee38e6f64bd23e
BANYU AJI SENJANA <020002618@sonia.net>
2026-01-07 19:50:43 +0700

    updated

--- config/dbQuery.js
+++ config/dbQuery.js
@@ -15,43 +15,6 @@ const dialect = process.env["DB_DIALECT_" + capsEnv] ?? process.env.DB_DIALECT ?
 const port = process.env["DB_PORT_" + capsEnv] ?? process.env.DB_PORT ?? 3306
 
 
-
-// const env = process.env.NODE_ENV
-
-// if (env === 'development' || env === 'test'){
-//   require("dotenv").config()
-// }
-
-// const capsEnv = env.toUpperCase() 
-
-// const username = process.env["DB_USERNAME_" + capsEnv]
-// const password = process.env["DB_PASSWORD_" + capsEnv]
-// const database = process.env["DB_NAME_" + capsEnv]
-// const host = process.env["DB_HOST_" + capsEnv]
-// const dialect = process.env["DB_DIALECT_" + capsEnv]
-// const port = process.env["DB_PORT_" + capsEnv]
-
-// console.log(process.env.DB_DATABASE)
-
-// const sequelize = new Sequelize(
-//   process.env.DB_DATABASE,
-//   process.env.DB_USERNAME,
-//   process.env.DB_PASSWORD,
-//   {
-//     dialect: 'postgres',
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT,
-//     dialectOptions: {
-//       useUTC: false,
-//       dateStrings: true,
-//       typeCast: true
-//     },
-//     timezone: 'Asia/Jakarta',
-//     poolmax: 10
-//   },
-
-// );
-
 // Gunakan nilai dari .env, bukan hardcoded
 // Pastikan host menggunakan IPv4 (127.0.0.1) bukan localhost untuk menghindari IPv6
 const finalHost = host === 'localhost' ? '127.0.0.1' : host;
